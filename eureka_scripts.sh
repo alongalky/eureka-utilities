f1495ac17336480d19a93d3041d9b3422c963c3a
Andrea Villa <andreakarimodm@gmail.com>
2017-04-30 16:19:20 +0300

    Rename eureka_scripts.sh query parameter from machine_id to vm_id

--- eureka_scripts.sh
+++ eureka_scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-machinename=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/name")
-eureka_endpoint=$(echo 'y' | gcloud compute instances describe $machinename 2>&1 | grep -A 1 eureka_endpoint | grep value | awk '{print $2}')
-response=$(curl -s $eureka_endpoint/api/_internal/scripts?machine_id=$machinename)
+vm_id=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/name")
+eureka_endpoint=$(echo 'y' | gcloud compute instances describe $vm_id 2>&1 | grep -A 1 eureka_endpoint | grep value | awk '{print $2}')
+response=$(curl -s $eureka_endpoint/api/_internal/scripts?vm_id=$vm_id)
 
 script=$(jq '.script' <<<$response | sed -r 's/^"(.*)"$/\1/')
 
