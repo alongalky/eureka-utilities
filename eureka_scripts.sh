ff9a46b043b5f4aa05912e7ab4d3943d3e1a88ef
Andrea Villa <andreakarimodm@gmail.com>
2017-09-05 14:57:09 +0300

    eureka_script.sh as currently on machinas

--- eureka_scripts.sh
+++ eureka_scripts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 vm_id=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/name")
-eureka_endpoint=$(echo 'y' | gcloud compute instances describe $vm_id 2>&1 | grep -A 1 eureka_endpoint | grep value | awk '{print $2}')
+eureka_endpoint=$(echo 'y' | gcloud compute instances describe --zone us-east1-b $vm_id 2>&1 | grep -A 1 eureka_endpoint | grep value | awk '{print $2}')
 response=$(curl -s $eureka_endpoint/api/_internal/scripts?vm_id=$vm_id)
 
-script=$(jq '.script' <<<$response | sed -r 's/^"(.*)"$/\1/' | sed 's/\\"/"/g')
+script=$(jq '.script' <<<$response | sed -r 's/^"(.*)"$/\1/')
 
 echo -e $script >/tmp/eureka_script.sh
 bash /tmp/eureka_script.sh &
