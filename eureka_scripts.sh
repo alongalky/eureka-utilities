05a7639661fe41acd7a9978d31c9fb88be85311c
Alon Gal <alongalky@gmail.com>
2017-09-25 16:32:51 +0300

    Bugfix: missing sed

--- eureka_scripts.sh
+++ eureka_scripts.sh
@@ -4,7 +4,7 @@ vm_id=$(curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/co
 eureka_endpoint=$(echo 'y' | gcloud compute instances describe --zone us-east1-b $vm_id 2>&1 | grep -A 1 eureka_endpoint | grep value | awk '{print $2}')
 response=$(curl -s $eureka_endpoint/api/_internal/scripts?vm_id=$vm_id)
 
-script=$(jq '.script' <<<$response | sed -r 's/^"(.*)"$/\1/')
+script=$(jq '.script' <<<$response | sed -r 's/^"(.*)"$/\1/' | sed 's/\\"/"/g')
 
 echo -e $script >/tmp/eureka_script.sh
 bash /tmp/eureka_script.sh &
